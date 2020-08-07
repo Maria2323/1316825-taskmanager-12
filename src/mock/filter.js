ef2775b2e03f99364efe3eaa3ea9fe19929cd844
Maria2323 <manehka_ilu@mail.ru>
2020-08-07 11:14:38 +0300

    Добавляет mock.js

---
+++ src/mock/filter.js
@@ -0,0 +1,28 @@
+import {isExpired, isRepeating, isExpiringToday} from "./../util.js";
+
+const taskToFilterMap = {
+  all: (tasks) => tasks.filter((task) => !task.isArchive).length,
+  overdue: (tasks) => tasks
+    .filter((task) => !task.isArchive)
+    .filter((task) => isExpired(task.dueDate)).length,
+  today: (tasks) => tasks
+    .filter((task) => !task.isArchive)
+    .filter((task) => isExpiringToday(task.dueDate)).length,
+  favorites: (tasks) => tasks
+    .filter((task) => !task.isArchive)
+    .filter((task) => task.isFavorite).length,
+  repeating: (tasks) => tasks
+    .filter((task) => !task.isArchive)
+    .filter((task) => isRepeating(task.repeatingDays)).length,
+  archive: (tasks) => tasks.filter((task) => task.isArchive).length,
+};
+
+
+export const generateFilter = (tasks) => {
+  return Object.entries(taskToFilterMap).map(([filterName, countTasks]) => {
+    return {
+      name: filterName,
+      count: countTasks(tasks),
+    };
+  });
+};
