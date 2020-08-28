cc37ebb6d702068c647213f4ee5e1aaf2664c38f
Maria2323 <manehka_ilu@mail.ru>
2020-08-28 10:33:07 +0300

    Добавляет класс board, переносит в board код из main

---
+++ src/utils/task.js
@@ -0,0 +1,30 @@
+export const isRepeating = (repeatingDays) => {
+  return Object.values(repeatingDays).some(Boolean);
+};
+
+export const humanizeTaskDueDate = (dueDate) => {
+  return dueDate.toLocaleString(`en-US`, {day: `numeric`, month: `long`});
+};
+
+export const isExpired = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  let currentDate = new Date();
+  currentDate.setHours(23, 59, 59, 999);
+  currentDate = new Date(currentDate);
+
+  return currentDate.getTime() > dueDate.getTime();
+};
+
+export const isExpiringToday = (dueDate) => {
+  if (dueDate === null) {
+    return false;
+  }
+
+  let currentDate = new Date();
+  currentDate.setHours(23, 59, 59, 999);
+
+  return currentDate.getTime() === dueDate.getTime();
+};
